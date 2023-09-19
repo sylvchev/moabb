5e9ea0e6a875a209a215c2869cf81d4d97c12cac
Sylvain Chevallier <sylain.chevallier@universite-paris-saclay.fr>
2023-09-19 07:39:43 +0200

    add scription for jean zay

---
+++ moabb-jeanzay-cpu-mi-rf.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --job-name=mobatch # nom du job
+#SBATCH --output=/gpfsdswork/projects/rech/ouw/uhi38pi/logs/benchmark-%x-%j.log # fichier de sortie (%j = job ID, %x = job name)
+#SBATCH --error=/gpfsdswork/projects/rech/ouw/uhi38pi/logs/error-%x-%j.log # fichier d’erreur (%j = job ID, %x = job name)
+# #SBATCH --constraint=v100-16g # demander des GPU a 16 Go de RAM, sinon a100
+#SBATCH --nodes=2 # reserver 1 nœud
+#SBATCH --ntasks=8 # nombre de sujet
+# #SBATCH --gres=gpu:1 # reserver 4 GPU, si CPU alors pas gpu
+#SBATCH --cpus-per-task=10 # reserver 10 CPU par tache (et memoire associee)
+#SBATCH --time=2:00:00 # temps maximal d’allocation "(HH:MM:SS)"
+#SBATCH --qos=qos_cpu-dev # QoS qos_cpu-t3, qos_cpu-t4, qos_cpu-dev, idem avec gpu
+#SBATCH --hint=nomultithread # desactiver l’hyperthreading
+#SBATCH --account=ouw@cpu # comptabilite V100
+
+# set -x # activer l’echo des commandes
+module purge # nettoyer les modules herites par default
+# conda deactivate # desactiver les environnements herites par default
+module load tensorflow-gpu/py3/2.11.0 # charger les modules
+
+srun python -u $HOME/src/benchmark-moabb-bnci-rhf.py # executer son script
+
+nb_cpu=10
+in_file="dataset-AlexandreMotorImagery.txt"
+save_dir="/gpfswork/rech/ouw/uhi38pi"
+pipelines="/linkhome/rech/genrqo01/uhi38pi/src/github/moabb/pipelines-mi"
+context="/linkhome/rech/genrqo01/uhi38pi/src/github/moabb/contexts/mi_rh_f.yml"
+
+# task is $SLURM_ARRAY_TASK_ID
+
+while read line; do
+	 dataset=$(echo ${line} | awk '{ print $1 }')
+	 subj=$(echo ${line} | awk '{print $2}')
+
+	 # create subj folder if it does not exist
+	 if [ ! -e ${save_dir}/${dataset}-${subj} ]; then
+		 mkdir -p ${save_dir}/${dataset}-${subj}/results ${save_dir}/${dataset}-${subj}/benchmark
+	 fi
+	 srun python -u run-jeanzay.py --pipelines ${pipelines} --evaluations "WithinSession" --results ${save_dir}/${dataset}-${subj}/results --output ${save_dir}/${dataset}-${subj}/benchmark --threads ${nb_cpu} --contexts ${context} --include-dataset ${dataset} --subject ${subjt}
+done < ${in_file}
+wait
---
+++ moabb/run-jeanzay.py
@@ -0,0 +1,150 @@
+import logging
+from argparse import ArgumentParser
+
+import mne
+
+from moabb.benchmark import benchmark
+from moabb.datasets import utils
+from moabb.utils import set_download_dir
+
+
+log = logging.getLogger(__name__)
+
+
+def parser_init():
+    parser = ArgumentParser(description="Main run script for MOABB")
+
+    parser.add_argument(
+        "-mne_p",
+        "--mne_data",
+        dest="mne_data",
+        type=str,
+        default=None,
+        help="Folder where to save and load the datasets with mne structure.",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--pipelines",
+        dest="pipelines",
+        type=str,
+        default="./pipelines/",
+        help="Folder containing the pipelines to evaluates.",
+    )
+    parser.add_argument(
+        "-e",
+        "--evaluations",
+        dest="evaluations",
+        type=list,
+        default=None,
+        help="Evaluation types to be run. Must be given as a list. "
+        'Options - ["WithinSession","CrossSession","CrossSubject"]'
+        "By default, all 3 types of evaluations will be done",
+    )
+    parser.add_argument(
+        "-r",
+        "--results",
+        dest="results",
+        type=str,
+        default="./results/",
+        help="Folder to store the results.",
+    )
+    parser.add_argument(
+        "-f",
+        "--force-update",
+        dest="force",
+        action="store_true",
+        default=False,
+        help="Force evaluation of cached pipelines.",
+    )
+
+    parser.add_argument(
+        "-v", "--verbose", dest="verbose", action="store_true", default=False
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        dest="debug",
+        action="store_true",
+        default=False,
+        help="Print debug level parse statements. Overrides verbose",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="output",
+        type=str,
+        default="./",
+        help="Folder to put analysis results",
+    )
+    parser.add_argument(
+        "--threads", dest="threads", type=int, default=1, help="Number of threads to run"
+    )
+    parser.add_argument(
+        "--plot",
+        dest="plot",
+        action="store_true",
+        default=False,
+        help="Plot results after computing. Defaults false",
+    )
+    parser.add_argument(
+        "-c",
+        "--contexts",
+        dest="context",
+        type=str,
+        default=None,
+        help="File path to context.yml file that describes context parameters."
+        "If none, assumes all defaults. Must contain an entry for all "
+        "paradigms described in the pipelines",
+    )
+    parser.add_argument(
+        "-i",
+        "--include-dataset",
+        dest="dataset",
+        type=str,
+        default=None,
+        help="Dataset to process",
+    )
+    parser.add_argument(
+        "-s",
+        "--subject",
+        dest="subject",
+        type=str,
+        default=None,
+        help="Subject to process in dataset",
+    )
+    return parser
+
+
+if __name__ == "__main__":
+    # FIXME: The verbose and debug params are useless currently
+    # set logs
+    mne.set_log_level(False)
+    # logging.basicConfig(level=logging.WARNING)
+
+    parser = parser_init()
+    options = parser.parse_args()
+
+    if options.mne_data is not None:
+        set_download_dir(options.mne_data)
+
+    include_datasets = [options.dataset]
+    datasets = utils.dataset_search(paradigm="imagery")
+    datasets_codes = [d.code for d in datasets]
+    for incdat in include_datasets:
+        if incdat in datasets_codes:
+            d = datasets[datasets_codes.index(incdat)]
+    d.subject_list = [options.subject]
+
+    # call within session benchmark
+    benchmark(
+        pipelines=options.pipelines,
+        evaluations=options.evaluations,
+        results=options.results,
+        overwrite=options.force,
+        output=options.output,
+        n_jobs=options.threads,
+        plot=options.plot,
+        contexts=options.context,
+        include_datasets=[d],
+    )
